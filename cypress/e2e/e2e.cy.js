c67e4de11fdda39b1cd4a0f3bcce8383ff37fc26
Veeti Laine <veeti.laine@tuni.fi>
2024-07-08 18:26:12 +0300

    11.9 complete

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -5,4 +5,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('a pokemon page can be opened and contains proper content', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
