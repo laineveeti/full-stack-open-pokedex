c2db7322ec452a2646b9fa443afcdba1e4b60560
Veeti Laine <veeti.laine@tuni.fi>
2024-07-08 18:19:42 +0300

    11.9

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents() {},
+    baseUrl: 'http://localhost:5000',
+    supportFile: false
+  },
+})
