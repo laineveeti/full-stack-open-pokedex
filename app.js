3a9956b8626d76eaad114e99f5dab4e8bb83b535
Veeti Laine <veeti.laine@tuni.fi>
2024-07-11 20:13:54 +0300

    still broke but should work now

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.git('/version', (req, res) => {
+app.get('/version', (req, res) => {
   res.send('0.0.2')
 })
 
-app.get('/health', (req, res) => {
+app.git('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
   //if (true) throw('error...  ')
   res.send('ok')
